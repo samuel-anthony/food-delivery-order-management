cc204265685780b2b19b65dacd896330274dff1a
minatorisato <36133909+minatorisato@users.noreply.github.com>
2020-02-03 23:24:03 +0700

    ui

---
+++ app/src/main/java/com/example/toko/orderEntry.java
@@ -0,0 +1,13 @@
+package com.example.toko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class orderEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_entry);
+    }
+}
---
+++ app/src/main/java/com/example/toko/topUpEntry.java
@@ -0,0 +1,13 @@
+package com.example.toko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class topUpEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up_entry);
+    }
+}
