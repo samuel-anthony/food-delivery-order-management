4f671074c34a9176e33ad806bc83dcfb3b161e03
minatorisato <36133909+minatorisato@users.noreply.github.com>
2020-03-24 00:24:00 +0700

    login tes

--- app/src/main/java/com/example/toko/ConfigURL.java
+++ app/src/main/java/com/example/toko/ConfigURL.java
@@ -3,7 +3,8 @@ package com.example.toko;
 public class ConfigURL {
     //ini buat constant yang di pake di db nantinya
     //ip wifi arenda =  192.168.0.108
-    private final static String staticIP = "192.168.0.108";
+    private final static String staticIP = "192.168.0.105";
     private final static String databaseName = "toko";
     public final static String Login = "http://" + staticIP + "/"+databaseName+"/login.php/";
+    public final static String Register = "http://" + staticIP + "/"+databaseName+"/register.php/";
 }
--- app/src/main/java/com/example/toko/LoginRegister.java
+++ app/src/main/java/com/example/toko/LoginRegister.java
@@ -50,19 +50,21 @@ public class LoginRegister extends AppCompatActivity {
             ft.commit();
         }
         else if(view == findViewById(R.id.buttonLogin)){
-            Intent mainMenu = new Intent(this,MainActivity.class);
-            mainMenu.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(mainMenu);
-            Toast.makeText(LoginRegister.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+//            Intent mainMenu = new Intent(this,MainActivity.class);
+//            mainMenu.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(mainMenu);
+//            Toast.makeText(LoginRegister.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+            checkLogin(this);
         }
         else if(view == findViewById(R.id.buttonRegister)){
             //harusnya nanti ada validasinya
-            Fragment fragment = new login();
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.fragmentloginRegister, fragment);
-            ft.commit();
-            Toast.makeText(LoginRegister.this,"Register Successfully",Toast.LENGTH_SHORT).show();
+            String usernameString = ((EditText)findViewById(R.id.usernameRegister)).getText().toString();
+            String phoneString = ((EditText)findViewById(R.id.phoneRegister)).getText().toString();
+            String passwordString = ((EditText)findViewById(R.id.passwordRegister)).getText().toString();
+            if(!usernameString.isEmpty() && !phoneString.isEmpty() && !passwordString.isEmpty())
+                checkRegister(this);
+            else
+                Toast.makeText(LoginRegister.this,"Mohon masukan semua data",Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -94,7 +96,7 @@ public class LoginRegister extends AppCompatActivity {
                         Intent mainActivity = null;
                         if(output.getString("is_user").equalsIgnoreCase("1")){
                             mainActivity = new Intent(context, MainActivity.class);
-                            mainActivity.putExtra("employee_data",s);
+                            mainActivity.putExtra("user_data",s);
                         }
                         else{
                             mainActivity = new Intent(context,adminMainActivity.class);
@@ -111,14 +113,70 @@ public class LoginRegister extends AppCompatActivity {
             @Override
             protected String doInBackground(Void... v) {
                 HashMap<String,String> params = new HashMap<>();
-                String email = ((EditText)findViewById(R.id.usernameLogin)).getText().toString();
+                String username = ((EditText)findViewById(R.id.usernameLogin)).getText().toString();
                 String password = ((EditText)findViewById(R.id.passwordLogin)).getText().toString();
 
-                params.put("email",email);
+                params.put("username",username);
                 params.put("password",password);
 
                 RequestHandler rh = new RequestHandler();
                 String res = rh.sendPostRequest(ConfigURL.Login, params);
+                return res;
+            }
+        }
+
+        checkLoginToDB ae = new checkLoginToDB(context);
+        ae.execute();
+    }
+
+    public void checkRegister(Context context){
+        class checkLoginToDB extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+            Context context;
+
+            public checkLoginToDB(Context context){
+                this.context = context;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(context,"Mencoba Login...","Please wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                try {
+                    JSONObject output = new JSONObject(s);
+                    if(output.getString("value").equalsIgnoreCase("1")){
+                        Fragment fragment = new login();
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.replace(R.id.fragmentloginRegister, fragment);
+                        ft.commit();
+                    }
+                    Toast.makeText(context,output.getString("message"),Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                String username = ((EditText)findViewById(R.id.usernameRegister)).getText().toString();
+                String phone = ((EditText)findViewById(R.id.phoneRegister)).getText().toString();
+                String password = ((EditText)findViewById(R.id.passwordRegister)).getText().toString();
+
+                params.put("username",username);
+                params.put("phone_number",phone);
+                params.put("password",password);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(ConfigURL.Register, params);
                 return res;
             }
         }
