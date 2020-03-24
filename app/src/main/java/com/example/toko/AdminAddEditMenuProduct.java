a5085cc3da0b3a3af8d9c99f4959088ceb5b091d
minatorisato <36133909+minatorisato@users.noreply.github.com>
2020-03-25 02:50:48 +0700

    add new menu admin page

--- app/src/main/java/com/example/toko/AdminAddEditMenuProduct.java
+++ app/src/main/java/com/example/toko/AdminAddEditMenuProduct.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class adminMainActivity extends AppCompatActivity {
+public class AdminAddEditMenuProduct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_main);
+        setContentView(R.layout.activity_admin_add_edit_menu_product);
     }
 
-    public void changeMenu(View view) {
-        if(view == findViewById(R.id.buttonChangeMenuProduct)) {
+    public void addNewProduct(View view){
+        if(view == findViewById(R.id.addMenuProductText)||view == findViewById(R.id.addMenuProductButton)) {
             Intent mainActivity = null;
             mainActivity = new Intent(this, AdminAddEditMenuProduct.class);
             startActivity(mainActivity);
