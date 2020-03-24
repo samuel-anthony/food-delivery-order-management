4127405d3195bbb6b70a45ef98f7438cdcae75e4
minatorisato <36133909+minatorisato@users.noreply.github.com>
2020-03-25 00:45:58 +0700

    admin add product menu

--- app/src/main/java/com/example/toko/adminMainActivity.java
+++ app/src/main/java/com/example/toko/adminMainActivity.java
@@ -1,7 +1,9 @@
 package com.example.toko;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class adminMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class adminMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+    }
+
+    public void changeMenu(View view) {
+        if(view == findViewById(R.id.buttonChangeMenuProduct)) {
+            Intent mainActivity = null;
+            mainActivity = new Intent(this, AdminAddMenuProduct.class);
+            startActivity(mainActivity);
+        }
     }
 }
